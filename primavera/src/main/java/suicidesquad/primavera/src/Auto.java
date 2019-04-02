be5bab2ede82c332ec46a338d3600bc2d76edd17
CristianMaldonado <maldonadocristian2010@hotmail.com>
2019-04-02 20:55:02 -0300

    Inyeccion de dependencias simple

--- primavera/src/main/java/suicidesquad/primavera/src/Auto.java
+++ primavera/src/main/java/suicidesquad/primavera/src/Auto.java
@@ -1,22 +1,20 @@
 package suicidesquad.primavera.src;
 
-import suicidesquad.primavera.annotations.Component;
 import suicidesquad.primavera.annotations.Injected;
 
-@Component
 public class Auto {
 
 	@Injected
-	private int ruedas; // TODO en este caso deberia ser una clase
-
-	public int getRuedas() {
-		return ruedas;
-	}
-
-	public void setRuedas(int ruedas) {
-		this.ruedas = ruedas;
-	}
+	private Rueda ruedas;
 	
+	public Rueda getRuedas() {
+		return this.ruedas;
+	}
+
+	@Override
+	public String toString() {
+		return "Auto [ruedas=" + ruedas + "]";
+	}
 	
 	
 }
--- primavera/src/main/java/suicidesquad/primavera/src/Factory.java
+++ primavera/src/main/java/suicidesquad/primavera/src/Factory.java
@@ -1,27 +1,48 @@
 package suicidesquad.primavera.src;
 
-
 import java.lang.annotation.Annotation;
-
-import suicidesquad.primavera.annotations.Component;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Factory {
 
     public static Object getObject(Class<?> classType) {
 
         try {
- 
+
+        	//Creo la instancia de la clase
+        	Object instance = classType.newInstance();
+
         	//Obtengo el annotation "Component"
-        	Annotation annotation = classType.getAnnotation(Component.class);
+//        	Annotation annotation = classType.getAnnotation(Component.class);
+
+			//Obtengo los atributos publicos y privados de la clase
+        	for (Field field : classType.getDeclaredFields()) {
+				//Obtengo los annotatiosn de los atributos
+				for (Annotation annotation : field.getDeclaredAnnotations()) {
+
+					String annotationName = annotation.annotationType().getSimpleName();
+					
+					//Si tiene @Injected
+					if(annotationName.equals("Injected")) {
+						
+						//Le doy acceso para modificar el atributo ya que es privado
+						field.setAccessible(true);
+						
+						//Seteo una nueva instancia que se genera de forma recursiva
+						field.set(instance, Factory.getObject(field.getType()));
+					}
+					
+//					for (Method methods : annotation.annotationType().getMethods()) {
+//						
+//					//Los metodos que me devuelve son las variables del annotation ej. "Count"
+//						System.out.println("Ann methods: " + methods.getName()); 
+//					}
+				}
+			}
+        	return instance;
         	
-        	if(annotation != null) {
-        		return classType.newInstance();	
-        	} else {
-        		// No tiene sentido pero por el momento lo dejo asi, tenemos que ver este tema, si se debe arrojar una exception
-        		// o retonar algo distinto
-        		return null;
-        	}
-        				
+        	
 		} catch (InstantiationException | IllegalAccessException e) {
 			e.printStackTrace();
 			return null;
